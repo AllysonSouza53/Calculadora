0a2e33a5bf116119403ac110a5d85c31c2dc13dc
Usuario <Usuario@CPD03-M18>
2024-04-25 16:00:56 -0300

    25042024

--- MauiAppCalculadora/MainPage.xaml.cs
+++ MauiAppCalculadora/MainPage.xaml.cs
@@ -2,26 +2,50 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        String? operação = null;
+
+        double MPreOp = 0;
+        double MPostOp = 0;
+        double V = 0;
+
+        string MemV = "";
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void zerar_Clicked(object sender, EventArgs e)
+        private void removedor_de_sinais_no_visor()
         {
-
+            if (MemV == "+" ||
+                 MemV == "-" ||
+                 MemV == "/" ||
+                 MemV == "*")
+            {
+                MemV = "";
+            }
+        }
+        private void Zerar_Clicked(object sender, EventArgs e)
+        {
+            visor.Text = "0";
+            MemV = "";
+            MPreOp = 0;
+            MPostOp = 0;
+            operação = null;
         }
 
-        private void maismenos_Clicked(object sender, EventArgs e)
+        private void Maismenos_Clicked(object sender, EventArgs e)
         {
-
+            V = Convert.ToDouble(MemV);
+            V = V * -1;
+            visor.Text = V.ToString() ;
         }
 
-        private void porcento_Clicked(object sender, EventArgs e)
+        private void Porcento_Clicked(object sender, EventArgs e)
         {
-
+            V = Convert.ToDouble(MemV);
+            V = V/100;
+            visor.Text = V.ToString();
         }
 
         private void dividir_Clicked(object sender, EventArgs e)
@@ -31,17 +55,20 @@
 
         private void num7_Clicked(object sender, EventArgs e)
         {
-
+            MemV = num7.Text;
+            visor.Text= MemV;
         }
 
         private void num8_Clicked(object sender, EventArgs e)
         {
-
+            MemV = num8.Text;
+            visor.Text = MemV;
         }
 
         private void num9_Clicked(object sender, EventArgs e)
         {
-
+            MemV = num9.Text;
+            visor.Text = MemV;
         }
 
         private void multiplicacao_Clicked(object sender, EventArgs e)
@@ -51,16 +78,19 @@
 
         private void num4_Clicked(object sender, EventArgs e)
         {
-
+            MemV = num4.Text;
+            visor.Text = MemV;
         }
 
         private void num5_Clicked(object sender, EventArgs e)
         {
-
+            MemV = num5.Text;
+            visor.Text = MemV;
         }
         private void num6_Clicked(object sender, EventArgs e)
         {
-
+            MemV = num6.Text;
+            visor.Text = MemV;
         }
 
         private void menos_Clicked(object sender, EventArgs e)
@@ -70,17 +100,20 @@
 
         private void num_1_Clicked(object sender, EventArgs e)
         {
-
+            MemV = num1.Text;
+            visor.Text = MemV;
         }
 
         private void num_2_Clicked(object sender, EventArgs e)
         {
-
+            MemV = num2.Text;
+            visor.Text = MemV;
         }
 
         private void num_3_Clicked(object sender, EventArgs e)
         {
-
+            MemV = num3.Text;
+            visor.Text = MemV;
         }
 
         private void mais_Clicked(object sender, EventArgs e)
@@ -90,7 +123,8 @@
 
         private void num_0_Clicked(System.Object sender, System.EventArgs e)
         {
-
+            MemV = num0.Text;
+            visor.Text = MemV;
         }
 
         private void ponto_Clicked(System.Object sender, System.EventArgs e)
